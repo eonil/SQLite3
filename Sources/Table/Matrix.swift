b400bfb3a4f6ce3fecd2325177c660df32b92237
Hoon H <drawtree@me.com>
2014-11-01 23:58:23 +0900

    Commented duplicated symbol.

--- Sources/Table/Matrix.swift
+++ Sources/Table/Matrix.swift
@@ -1,33 +1,33 @@
+////
+////  Matrix.swift
+////  EonilSQLite3
+////
+////  Created by Hoon H. on 11/1/14.
+////
+////
 //
-//  Matrix.swift
-//  EonilSQLite3
+//import Foundation
 //
-//  Created by Hoon H. on 11/1/14.
-//
-//
-
-import Foundation
-
-struct Matrix {
-	let	columns:[String]
-	let	rows:[Row]
-}
-
-struct Row {
-	let	keys:[Value]
-	let	values:[Value]
-}
-
-
-
-
-
-
-
-
-//struct LazyMatrix {
-//	let	s:Statement
+//struct Matrix {
+//	let	columns:[String]
+//	let	rows:[Row]
 //}
-//struct LazyRow {
-//	let	s:Statement
+//
+//struct Row {
+//	let	keys:[Value]
+//	let	values:[Value]
 //}
+//
+//
+//
+//
+//
+//
+//
+//
+////struct LazyMatrix {
+////	let	s:Statement
+////}
+////struct LazyRow {
+////	let	s:Statement
+////}
